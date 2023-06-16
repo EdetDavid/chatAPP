dffa1f4ce43cef2c4cb5c21b8d38b360eca16442
Dvooskid <104776830+EdetDavid@users.noreply.github.com>
2023-06-16 01:33:17 +0100

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -32,9 +32,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap',
-    'fontawesome',
-    'ckeditor',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
