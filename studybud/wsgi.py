651cc5350f3386675b7be8691f4b0ba1b5201cd8
Dvooskid <104776830+EdetDavid@users.noreply.github.com>
2023-06-16 01:45:48 +0100

    Update wsgi.py

--- studybud/wsgi.py
+++ studybud/wsgi.py
@@ -11,39 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', '"""
-WSGI config for portfolio project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', '"""
-WSGI config for portfolio project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studybud.settings')
-
-application = get_wsgi_application()
-app = application.settings')
-
-application = get_wsgi_application()
-app = application.settings')
 
 application = get_wsgi_application()
 app = application
